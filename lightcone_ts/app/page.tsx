d75572bd46f06177bec10c658f55fc468d13929f
Marc Christophe <fusupo@gmail.com>
2024-12-30 14:19:22 -0800

    undo dummy change

--- lightcone_ts/app/page.tsx
+++ lightcone_ts/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from 'next/link';
 
 export default function Home() {
-  console.log("MUTHER FUCKING TOKEN",localStorage.getItem('token'));
   return (
     <div>
       <h1>Welcome to My App</h1>
