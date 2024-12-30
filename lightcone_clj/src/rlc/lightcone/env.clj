d31ebee67e99cb1a8dad148a6c611611adead6a2
Marc Christophe <fusupo@gmail.com>
2024-12-30 15:17:49 -0800

    update log satements

--- lightcone_clj/src/rlc/lightcone/env.clj
+++ lightcone_clj/src/rlc/lightcone/env.clj
@@ -1,18 +1,12 @@
-(ns rlc.lightcone.env
-  (:require [clojure.tools.logging :as log]))
+(ns rlc.lightcone.env)
 
-(do
-  (log/info "ARCHIVIST_SERVICE_URL from env:" (System/getenv "ARCHIVIST_SERVICE_URL"))
-  (log/info "CLARITY_SERVICE_URL from env:" (System/getenv "CLARITY_SERVICE_URL")))
+(print (str "CLARITY_SERVICE_URL: " (System/getenv "CLARITY_SERVICE_URL")))
+(print (str "ARCHIVIST_SERVICE_URL: " (System/getenv "ARCHIVIST_SERVICE_URL")))
 
 (def ARCHIVIST_SERVICE_URL
-  (let [url (or (System/getenv "ARCHIVIST_SERVICE_URL")
-                "http://localhost:3000")]
-    (log/info "Using ARCHIVIST_SERVICE_URL:" url)
-    url))
+  (or (System/getenv "ARCHIVIST_SERVICE_URL")
+      "http://localhost:3000"))
 
 (def CLARITY_SERVICE_URL
-  (let [url (or (System/getenv "CLARITY_SERVICE_URL")
-                "http://localhost:3001")]
-    (log/info "Using CLARITY_SERVICE_URL:" url)
-    url))
+  (or (System/getenv "CLARITY_SERVICE_URL")
+      "http://localhost:3001"))
