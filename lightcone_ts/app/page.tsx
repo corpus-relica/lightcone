6102b4634b3abe7b01a7770d11008c11fc87a34e
Marc Christophe <fusupo@gmail.com>
2024-12-30 13:29:46 -0800

    Log token retrieval in Home component for debugging purposes

--- lightcone_ts/app/page.tsx
+++ lightcone_ts/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from 'next/link';
 
 export default function Home() {
+  console.log("MUTHER FUCKING TOKEN",localStorage.getItem('token'));
   return (
     <div>
       <h1>Welcome to My App</h1>
