afeb7df9b8890768d7888e95b121cd433c7778c1
Marc Christophe <fusupo@gmail.com>
2024-12-30 15:24:21 -0800

    try temporary hardcode the target auth endpoint

--- lightcone_ts/app/login/page.tsx
+++ lightcone_ts/app/login/page.tsx
@@ -40,7 +40,8 @@ export default function LoginForm() {
 
     try {
       // Send login request to your Clojure backend
-      const response = await fetch("http://localhost:3003/login", {
+      // const response = await fetch("http://localhost:3003/login", {
+      const response = await fetch("http://64.23.130.139:3003/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
