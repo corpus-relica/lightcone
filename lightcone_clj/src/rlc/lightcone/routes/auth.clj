bfe769f2b6d30d0cd80e581e539c2b69b7a8e100
Marc Christophe <fusupo@gmail.com>
2024-12-30 23:51:15 -0800

    Refactor authentication routes in Lightcone server: remove user registration and login logic, replace with basic response for login endpoint. This change simplifies the routes for testing purposes and prepares for future enhancements.

--- lightcone_clj/src/rlc/lightcone/routes/auth.clj
+++ lightcone_clj/src/rlc/lightcone/routes/auth.clj
@@ -10,38 +10,10 @@
             [rlc.lightcone.env :refer [CLARITY_SERVICE_URL]]))
 
 
+;; In routes/auth.clj
 (defroutes auth-routes
-  (OPTIONS "/register" _ (response/response nil))
-  (POST "/register" request
-    (let [username (get-in request [:body "username"])
-          password (get-in request [:body "password"])]
-      (tap> "REGISTER")
-      (tap> username)
-      (tap> password)
-      (if (db-get-user-by-username username)
-        {:status 400 :body "Username already exists"}
-        (do
-          (create-user username password)
-          {:status 201 :body "User created successfully"}))))
-
   (OPTIONS "/login" _ (response/response nil))
   (POST "/login" request
-    (let [username (get-in request [:body "username"])
-          password (get-in request [:body "password"])]
-      (tap> "LOGIN, FOOL!")
-      (tap> (str "username : " username))
-      (tap> (str "password : " password))
-
-      (tap> "CLARITY SERVICE URL")
-      (tap> CLARITY_SERVICE_URL)
-
-      (if-let [user (authenticate-user username password)]
-        (do
-          (tap> "FONOFDSFDSFDSFDSF")
-          (tap> user)
-          {:status 200 :body {:token user}})
-        {:status 401 :body "Invalid credentials"})))
-
-  (POST "/logout" []
-    (-> (response/redirect "/")
-        (assoc :session nil))))
+    ;; First, just return a test response to verify basic routing
+    (tap> "Login attempt received")
+    (response/response {:message "Login endpoint reached"})))
