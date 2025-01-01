4ccabad0c4d644b88666f1d06b1ab863d68ba335
Marc Christophe <fusupo@gmail.com>
2025-01-01 07:36:07 -0800

    Refactor Lightcone server middleware: streamline CORS settings by removing unnecessary origins and methods, enhance JSON body parsing, and update server startup behavior for improved logging and responsiveness. This change simplifies the middleware stack and enhances server initialization feedback.

--- lightcone_clj/src/rlc/lightcone/server.clj
+++ lightcone_clj/src/rlc/lightcone/server.clj
@@ -4,48 +4,30 @@
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [rlc.lightcone.routes.core :refer [app-routes]]
             [ring.middleware.cors :refer [wrap-cors]]
-            [portal.api :as p]
+            [portal.api :as p]))
 
-[rlc.lightcone.io.auth :refer [authenticate-user
-                                        generate-token
-                                        db-get-user-by-username
-                                        create-user]]
-            ))
-
-;; Add middleware in the correct order
+;; Add middleware
 (def app
-  (-> #'app-routes  ; Note the var quote here
-      (wrap-cors
-       :access-control-allow-origin [#"http://localhost:3004"
-                                   #"http://64.23.130.139:3004"
-                                   #"http://localhost:3003"
-                                   #"http://64.23.130.139:3003"]
-       :access-control-allow-methods [:get :put :post :delete :options]
-       :access-control-allow-headers ["Content-Type" "Authorization"])  ; Added Authorization
-      (wrap-json-body {:keywords? true})  ; Add keywords? true
+  (-> app-routes
+      (wrap-cors :access-control-allow-origin [#"http://localhost:3004"
+                                               #"http://64.23.130.139:3004"]
+                :access-control-allow-methods [:get :put :post :delete :options])
+      wrap-json-body
       wrap-json-response))
 
 (defn start-server [port]
-  (jetty/run-jetty app {:port port  ; Remove the var quote here
-                        :join? false
-                        :max-header-size 65536}))
+  (jetty/run-jetty #'app {:port port
+                          :join? true
+                          :max-header-size 65536}))
 
 (defn -main [& args]
   ;; Start portal with web UI
   (p/open {:port 5555})
   (add-tap #'p/submit)
 
-  (tap> "Starting Lightcone server...foolishness")
-
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     (println "Starting server on port" port)
+    (start-server port)))
 
-    (start-server port)
-    (println "Server started on port" port)
 
-    (tap> "Started")
-      (if-let [user (authenticate-user "john" "changeme")]
-        (do
-          (tap> "FONOFDSFDSFDSFDSF")
-          (tap> user))
-        (tap> "Invalid credentials"))))
+(print "EAT SHIT")
