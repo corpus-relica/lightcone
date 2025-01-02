18fcbd60edf1f8755cf2da4e0605a688e54e65cb
Marc Christophe <fusupo@gmail.com>
2025-01-02 13:02:05 -0800

    Refactor Lightcone server middleware and enhance startup logging: Update CORS settings to include additional origins and authorization headers, improve JSON body parsing with keyword support, and modify server startup behavior for non-blocking operation. Added logging for server start confirmation and user authentication process, enhancing visibility and debugging capabilities.

--- lightcone_clj/src/rlc/lightcone/server.clj
+++ lightcone_clj/src/rlc/lightcone/server.clj
@@ -4,30 +4,48 @@
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [rlc.lightcone.routes.core :refer [app-routes]]
             [ring.middleware.cors :refer [wrap-cors]]
-            [portal.api :as p]))
+            [portal.api :as p]
 
-;; Add middleware
+[rlc.lightcone.io.auth :refer [authenticate-user
+                                        generate-token
+                                        db-get-user-by-username
+                                        create-user]]
+            ))
+
+;; Add middleware in the correct order
 (def app
-  (-> app-routes
-      (wrap-cors :access-control-allow-origin [#"http://localhost:3004"
-                                               #"http://64.23.130.139:3004"]
-                :access-control-allow-methods [:get :put :post :delete :options])
-      wrap-json-body
+  (-> #'app-routes  ; Note the var quote here
+      (wrap-cors
+       :access-control-allow-origin [#"http://localhost:3004"
+                                   #"http://64.23.130.139:3004"
+                                   #"http://localhost:3003"
+                                   #"http://64.23.130.139:3003"]
+       :access-control-allow-methods [:get :put :post :delete :options]
+       :access-control-allow-headers ["Content-Type" "Authorization"])  ; Added Authorization
+      (wrap-json-body {:keywords? true})  ; Add keywords? true
       wrap-json-response))
 
 (defn start-server [port]
-  (jetty/run-jetty #'app {:port port
-                          :join? true
-                          :max-header-size 65536}))
+  (jetty/run-jetty app {:port port  ; Remove the var quote here
+                        :join? false
+                        :max-header-size 65536}))
 
 (defn -main [& args]
   ;; Start portal with web UI
   (p/open {:port 5555})
   (add-tap #'p/submit)
 
+  (tap> "Starting Lightcone server...foolishness")
+
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     (println "Starting server on port" port)
-    (start-server port)))
 
+    (start-server port)
+    (println "Server started on port" port)
 
-(print "EAT SHIT")
+    (tap> "Started")
+      (if-let [user (authenticate-user "john" "changeme")]
+        (do
+          (tap> "FONOFDSFDSFDSFDSF")
+          (tap> user))
+        (tap> "Invalid credentials"))))
