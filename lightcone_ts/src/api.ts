be99c309f55711ffcf4c4a6d598db0c52bc10a1f
Marc Christophe <fusupo@gmail.com>
2025-01-09 23:39:33 -0800

    establish events api

--- lightcone_ts/src/api.ts
+++ lightcone_ts/src/api.ts
@@ -12,6 +12,36 @@ export const getEvents = async () => {
   return res.data;
 }
 
+export const createEvent = async (event: any) => {
+  const res = await authAxios().post('/api/events', event);
+  return res.data;
+}
+
+export const updateEvent = async (event: any) => {
+  const res = await authAxios().put(`/api/events/${event.id}`, event);
+  return res.data;
+}
+
+export const deleteEvent = async (id: number) => {
+  const res = await authAxios().delete(`/api/events/${id}`);
+  return res.data;
+}
+
+export const setEventTitle =async (id: number, title: string) => {
+  const res = await authAxios().put(`/api/events/${id}/title`, { title });
+  return res.data;
+}
+
+export const setEventNote = async (id: number, note: string) => {
+  const res = await authAxios().put(`/api/events/${id}/note`, { note });
+  return res.data;
+}
+
+export const setEventParticipants = async (id: number, participants: number[]) => {
+  const res = await authAxios().put(`/api/events/${id}/participants`, { participants });
+  return res.data;
+}
+
 export const getPeople = async () => {
   const res = await authAxios().get('/api/persons')
   return res.data.persons
